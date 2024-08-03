01fade63cb89229263010c5f0a2c117055a26e09
aayush maharjan <aayushmaharjan75@gmail.com>
2024-08-03 23:50:38 +0545

    minor

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,15 +1,12 @@
 import multer from "multer";
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // destination is used to specify the path of the directory in which the files have to be stored
     cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    // It is the filename that is given to the saved file.
     cb(null, file.originalname);
   },
 });
 
-// Configure storage engine instead of dest object.
 const upload = multer({ storage: storage });
 export default upload;
